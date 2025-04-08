6b08e4e975cc5b6bd5e15ed9f4e60f7dc2c91e8c
dinu141 <dinudileep0304@gmail.com>
2025-04-08 15:13:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function clicksubmitbutton(event){
-    const username=document.getELementById("username").ariaValueMax;
-    const welcomeMessage=document.getELementById('welcome-message');
-    welcomeMessage.textContent+'welcome,'+username +'!';
-    welcomeMessage.style.display='block';
+    const username = document.getElementById("username").ariaValueMax;
+    const welcomeMessage = document.getELementById('welcome-message');
+    welcomeMessage.textContent ='welcome,'+username +'!';
+    welcomeMessage.style.display ='block';
 }
